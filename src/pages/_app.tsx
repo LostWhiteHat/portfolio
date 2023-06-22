10c0bc40ef847860171a56c0e8e6ebc237b87b76
Lost <patrick.buerdel@edu.band.ch>
2023-06-22 14:52:53 +0200

    removed unused dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import '@/styles/globals.css'
-import {SessionProvider} from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <link rel="shortcut icon" href="./images/favicon.ico"></link>
-      </Head>
-      <Component {...pageProps} />
-    </SessionProvider>
+      <><Head>
+      <link rel="shortcut icon" href="./images/favicon.ico"></link>
+    </Head>
+    <Component {...pageProps} /></>
   )
 }
