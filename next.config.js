bdaf61ffa3653dcc829230f4e3361f1a90c2f2b7
Lost <patrick.buerdel@edu.band.ch>
2023-06-27 14:03:59 +0200

    changed next config for docker build and added .vscode to .dockerignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  output: 'standalone'
+}
