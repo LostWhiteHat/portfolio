20000461de6f03bcd74b69152f9af45b19c5782b
Lost <patrick.buerdel@edu.band.ch>
2023-06-27 13:31:14 +0200

    changed flex style of model contetn

--- src/pages/compontents/Modal/ProjectItem.tsx
+++ src/pages/compontents/Modal/ProjectItem.tsx
@@ -6,7 +6,7 @@ const ProjectItem: React.FC<Project> = ({title, description, link}) => {
     return (
         <>
             <motion.li className='mb-3'>
-                <div className='flex justify-between items-start flex-row mt-5'>
+                <div className='flex justify-between items-center flex-row mt-5'>
                     <h1 className='text-lg'><b>{title}</b></h1>
                     <p className='ml-8 mr-4'>{description}</p>
                     <a href={link} target='_blank'>
