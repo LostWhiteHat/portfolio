6dccdfe7e22f47666d0a8ce12f0c00e22b42f6bd
Lost <patrick.buerdel@edu.band.ch>
2023-11-07 15:23:17 +0100

    customized 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,34 @@
-import Waves from './compontents/waves/waves';
-
 export default function Custom404() {
     return (
-        <div className="fixed w-screen h-screen flex items-center justify-center z-[99] bg-secondary">
-            <h1>404</h1>
+        <div id="background" className="z-[98] fixed w-screen h-screen">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 285 80"
+                preserveAspectRatio="xMidYMid slice"
+                id="svg"
+            >
+                <defs>
+                    <mask id="mask" x="0" y="0" width="100%" height="100%">
+                        <rect
+                            x="0"
+                            y="0"
+                            width="100%"
+                            height="100%"
+                            fill="white"
+                        />
+                        <text id="line1" x="103" y="40">
+                            404
+                        </text>
+                        <text id="line2" x="70" y="60">
+                            PAGE NOT FOUND
+                        </text>
+                    </mask>
+                </defs>
+                <rect x="0" y="0" width="100%" height="100%" fill="black" />
+            </svg>
         </div>
     );
 }
