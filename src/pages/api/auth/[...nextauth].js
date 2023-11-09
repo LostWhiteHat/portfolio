b0db72e286d91eb64cc8ec4f19a4f8e8c222873e
Lost <patrick.buerdel@edu.band.ch>
2023-11-09 07:58:35 +0100

    added LinkedIn Provider for auth (Not tested)

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
+import LinkedinProvider from "next-auth/providers/linkedin";
 import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from "pg";
 
@@ -22,13 +23,17 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
+        LinkedinProvider({
+            clientId: process.env.LINKEDIN_ID,
+            clientSecret: process.env.LINKEDIN_SECRET
+        }),
         EmailProvider({
             server: {
                 host: process.env.EMAIL_SERVER_HOST,
                 port: process.env.EMAIL_SERVER_PORT,
                 auth: {
                     user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD
+                    pass: process.env.EMAIL_SERVER_PASS
                 }
             },
             from: process.env.EMAIL_FROM
