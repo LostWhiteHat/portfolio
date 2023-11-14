1ba0dde71c90aec3e9b094ff3283f699c89b9174
Lost <patrick.buerdel@edu.band.ch>
2023-11-14 08:17:40 +0100

    updated next config headers

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: ["placeholder.co"]
-//     }
-// }
-
-// module.exports = nextConfig
-
 module.exports = {
     output: 'standalone',
     headers: {
-        'Access-Control-Allow-Origin': ['https://buerdel.dev'],
+        'Access-Control-Allow-Origin': ['*'],
         'Access-Control-Allow-Methods': ['GET']
     }
 };
